379a5667757a4f9d743badc46a9670fb63c5501d
daxlar <azhang100@gmail.com>
2019-07-13 05:03:20 +0000

    added cache

--- server.cc
+++ server.cc
@@ -9,26 +9,51 @@
 #include <thread>
 #include <sys/types.h>
 #include <unistd.h>
+#include <unordered_set>
+#include <arpa/inet.h>
 
 #define PORT 8080
-
 #define MAXLINE 1024
-#define RECVBYTES 100
 
 
+std::unordered_set<std::string> user_cache;
+const char* coordinates = "1,1,1,1";
 
 void udp_handler(int fdtn)
 {
 
-	char  buffer[RECVBYTES];
+	char  buffer[MAXLINE];
+	char str[] = "hello";
+	char* ipstring;
 	struct sockaddr_in cliaddr;
 	int len;
+	int hello_check;
+
 	memset(&cliaddr, 0, sizeof(cliaddr));
 	while(true)
 	{
 
-		int bytes_recv = recvfrom(fdtn, (char*)buffer, RECVBYTES, MSG_WAITALL,
+		int bytes_recv = recvfrom(fdtn, (char*)buffer, MAXLINE, MSG_WAITALL,
 						(struct sockaddr*)&cliaddr, (socklen_t*)&len);
+		hello_check = memcmp(buffer, str, sizeof(str));
+		if(hello_check == 0)
+		{
+			sendto(fdtn,(const char*)coordinates,strlen(coordinates),MSG_CONFIRM,
+					(const struct sockaddr*)&cliaddr,len);
+			std::cout << "sending coordinates" << std::endl;
+		}
+		if(buffer[0] == '1')
+		{
+			ipstring = inet_ntoa(cliaddr.sin_addr);
+			printf("ip address is %s\n", ipstring);
+			std::string cache_str(ipstring);
+			if(user_cache.find(cache_str) == user_cache.end())
+			{
+				std::cout << "added user !" << std::endl;
+				user_cache.insert(cache_str);
+			}
+		}
+		
 		buffer[bytes_recv] = '\0';
 		printf("string recv: %s\n", buffer);
 
@@ -60,28 +85,6 @@ int main()
 		exit(0);
 	}
 
-	int len, n;
-	char str[] = "hello";
-	std::cout<<"now I'm recv" << std::endl;
-
-	int str_check = 1;
-	while(str_check != 0)
-	{
-		n = recvfrom(sockfd, (char*)buffer, MAXLINE, MSG_WAITALL,
-		(struct sockaddr *) &cliaddr, (socklen_t*)&len);
-		buffer[n] = '\0';
-		printf("client : %s\n", buffer);
-		str_check  = memcmp(buffer, str, sizeof(str));
-		std::cout << str_check << std::endl;
-		if(str_check == 0)
-		{
-
-			sendto(sockfd, (const char *)hello, strlen(hello),
-					MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
-						len);
-			std::cout << "i sent" << std::endl;	
-		}
-	}
 	std::cout << "making a thread" << std::endl;
 	std::thread t1(udp_handler, sockfd);
 	t1.join();
